a8b38ed7ff044f98463bfee1e6690db6631edca2
Rob Harrop <rob.harrop@gmail.com>
2011-11-23 12:42:53 +0000

    Added buildfile and Rakefile to ruby-mode auto list

--- emacs/ruby-config.el
+++ emacs/ruby-config.el
@@ -1,3 +1,5 @@
 (provide 'ruby-config)
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("buildfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.pp$" . ruby-mode))
