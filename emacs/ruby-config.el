3977f098b5b90266dd38c208158cdea1edb172b2
Rob Harrop <rob.harrop@gmail.com>
2011-12-08 13:44:41 +0000

    Added rvm-mode and yari-mode to emacs ruby configuration

--- emacs/ruby-config.el
+++ emacs/ruby-config.el
@@ -4,5 +4,19 @@
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.pp$" . ruby-mode))
 
+;; rhtml-mode
 (add-sub-path "rhtml-mode")
 (require 'rhtml-mode)
+
+;; rvm-mode
+(add-sub-path "rvm-mode")
+(require 'rvm)
+(rvm-use-default)
+
+;; yari
+(add-sub-path "yari-mode")
+(require 'yari)
+
+(defun ri-bind-key ()
+  (local-set-key (kbd "C-c r") 'yari))
+(add-hook 'ruby-mode-hook 'ri-bind-key)
