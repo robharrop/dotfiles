cb0ab9584e373051256fb8c957cb163768398e07
Rob Harrop <rharrop@accel.com>
2019-12-17 16:51:51 +0000

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+starship init fish | source
+
+status --is-interactive; and source (rbenv init -|psub)
+
+alias g='git'
+alias ga='git add'
+alias gc='git commit'
+alias gco='git checkout'
+alias gst='git status'
+alias gl='git pull'
+alias gp='git push'
+
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
