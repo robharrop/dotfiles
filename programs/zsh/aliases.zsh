5e7ee6434671c52ae55485ad7470ee5f9117f76d
Rob Harrop <rharrop@accel.com>
2020-11-11 15:33:50 +0000

    More aliases

--- programs/zsh/aliases.zsh
+++ programs/zsh/aliases.zsh
@@ -2,9 +2,13 @@ alias g='git'
 alias ga='git add'
 alias gc='git commit'
 alias gco='git checkout'
+alias gcm='git checkout master'
 alias gst='git status'
 alias gl='git pull'
 alias gp='git push'
+alias gpb='git push -u origin `git rev-parse --abbrev-ref HEAD`'
+
+alias prw='gh pr create -w'
 
 alias be='bundle exec'
 alias bi='bundle install'
@@ -14,3 +18,7 @@ alias dc='docker-compose'
 alias dcu='docker-compose up'
 alias dcd='docker-compose down'
 alias dcr='docker-compose run'
+
+alias yi='yarn install'
+
+alias ng='ngrok http -hostname=rharrop-sorcery.eu.ngrok.io -region eu'
